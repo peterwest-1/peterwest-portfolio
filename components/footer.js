4bf4c8845e2938095f041c92818bde86c1c92873
Pete West <10847430+peterwest-1@users.noreply.github.com>
2022-03-07 17:45:11 +0200

    Enabled ESLint

--- components/footer.js
+++ components/footer.js
@@ -13,6 +13,7 @@ export default function Footer() {
             <a
               href={LINKEDIN_LINK}
               target="_blank"
+              rel="noreferrer"
               className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               LinkedIn
@@ -20,6 +21,7 @@ export default function Footer() {
             <a
               href={GITHUB_LINK}
               target="_blank"
+              rel="noreferrer"
               className="mx-3 bg-white hover:bg-black hover:text-white border border-black text-black font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0"
             >
               GitHub
