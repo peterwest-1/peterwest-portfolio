ee8c67e4bc4f0190166c51e0a6e118a826201107
Pete West <peterwest86@gmail.com>
2022-03-08 09:11:29 +0200

    Abstracted schemas

--- pages/sketches.js
+++ pages/sketches.js
@@ -5,7 +5,7 @@ import Header from "../components/header";
 
 import React from "react";
 import dynamic from "next/dynamic";
-import { setup, draw } from "../sketches/quick-sort";
+import { setup, draw } from "../sketches/astar";
 
 // Will only import `react-p5` on client-side
 const Sketch = dynamic(() => import("react-p5").then((mod) => mod.default), {
@@ -25,6 +25,14 @@ export default function Sketches() {
             Sketches
           </h1>
           <Sketch setup={setup} draw={draw} />
+          {/* <div className="my-3">
+            <button className="mx-3 bg-black hover:bg-white hover:text-black border border-black text-white font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
+              Stop
+            </button>
+            <button className="mx-3 bg-white hover:bg-black hover:text-white border border-black text-black font-bold py-3 px-12 lg:px-8 duration-200 transition-colors mb-6 lg:mb-0">
+              Reset
+            </button>
+          </div> */}
         </Container>
       </Layout>
     </>
