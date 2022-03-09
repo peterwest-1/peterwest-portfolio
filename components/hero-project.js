f1290ea2feeb2887545cf86993d616b3b35106a6
Pete West <10847430+peterwest-1@users.noreply.github.com>
2022-03-09 17:35:54 +0200

    Formatting changes

--- components/hero-project.js
+++ components/hero-project.js
@@ -19,7 +19,7 @@ export default function HeroProject({
       <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
         <div>
           <h3 className="mb-4 text-4xl lg:text-6xl leading-tight">
-            <Link href={`/projects/${slug}`}>
+            <Link href={`/projects/[slug]`} as={`/projects/${slug}`}>
               <a className="hover:underline">{title}</a>
             </Link>
           </h3>
---
+++ schemas/author.js
@@ -0,0 +1,17 @@
+export default {
+  name: "author",
+  title: "Author",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      title: "Name",
+      type: "string",
+    },
+    {
+      name: "picture",
+      title: "Picture",
+      type: "image",
+    },
+  ],
+};
