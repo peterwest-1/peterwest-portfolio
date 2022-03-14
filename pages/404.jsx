eeb285a379f893e264387c8674057938bbd029dc
Pete West <10847430+peterwest-1@users.noreply.github.com>
2022-03-14 15:45:57 +0200

    ESLint changes for previous commit

--- pages/404.jsx
+++ pages/404.jsx
@@ -3,22 +3,20 @@ import Layout from "../components/layout";
 import Header from "../components/header";
 import Head from "next/head";
 import { TITLE } from "../lib/constants";
+import Nothing from "../components/nothing";
 
 export default function Custom404() {
-
   return (
     <Layout>
-        <Head>
-          <title>404 | {TITLE} </title>
-        </Head>
-        <Container>
-          <Header />
+      <Head>
+        <title>404 | {TITLE} </title>
+      </Head>
+      <Container>
+        <Header />
         <h1 className="text-6xl md:text-7xl lg:text-8xl font-bold tracking-tighter leading-tight md:leading-none mb-12 text-center md:text-left mt-8">
           404
         </h1>
-        <p className=" text-3xl md:text-4xl lg:text-5xl font-semibold tracking-tighter leading-tight md:leading-none mb-12  md:text-left">
-          Nothing here, I'm afraid.
-        </p>
+        <Nothing />
       </Container>
     </Layout>
   );
