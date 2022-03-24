f4888d52f8c52a59d0cfb7b9eb4d7da7fb9d30fc
Pete West <peterwest86@gmail.com>
2022-03-24 13:17:13 +0200

    Removed Skills description

--- components/skills.jsx
+++ components/skills.jsx
@@ -2,12 +2,11 @@ import SkillsMobile from "./skills-mobile";
 import SkillsWeb from "./skills-web";
 import SkillsGeneral from "./skills-general";
 
-//TODO: - Fix keys asap lmao
 export default function Skills() {
   return (
     <section>
       <h2 className="mb-8 text-6xl md:text-7xl font-bold tracking-tighter leading-tight">
-        Skills &gt; <a className="text-gray-500  font-normal ">Development &amp; Design</a>
+        Skills
       </h2>
       <div className="grid grid-cols-3 md:grid-cols-3 md:gap-x-4 lg:gap-x-8 gap-y-20 md:gap-y-32 mb-32">
         <SkillsMobile />
