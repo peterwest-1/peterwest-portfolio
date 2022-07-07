31b4af488f3f6e1a2f2f7394880d7a8ffdc2d51c
Pete West <peterwest86@gmail.com>
2022-07-07 11:51:49 +0200

    Attempting to fix issue with analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -7,19 +7,10 @@ function MyApp({ Component, pageProps }) {
   const routers = useRouter();
   useEffect(() => {
     if (process.env.NODE_ENV === "production") {
-      const logEvent = (url) => {
-        analytics().setCurrentScreen(url);
-        analytics().logEvent("screen_view");
-      };
-
-      routers.events.on("routeChangeComplete", logEvent);
-      //For First Page
-      logEvent(window.location.pathname);
-
-      //Remvove Event Listener after un-mount
-      return () => {
-        routers.events.off("routeChangeComplete", logEvent);
-      };
+      analytics.logScreenView({
+        screen_name: "SignUpScreen",
+        screen_class: "SignUpScreen",
+      });
     }
   }, []);
   return <Component {...pageProps} />;
