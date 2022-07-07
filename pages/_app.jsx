dbdc5f1f3b4ff77cf386ab195a9ad6d5262296b2
Pete West <peterwest86@gmail.com>
2022-07-07 11:41:35 +0200

    Added firebase analytics

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,7 +1,28 @@
-import '../styles/index.css'
+import { analytics } from "../lib/firebase";
+import "../styles/index.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const routers = useRouter();
+  useEffect(() => {
+    if (process.env.NODE_ENV === "production") {
+      const logEvent = (url) => {
+        analytics().setCurrentScreen(url);
+        analytics().logEvent("screen_view");
+      };
+
+      routers.events.on("routeChangeComplete", logEvent);
+      //For First Page
+      logEvent(window.location.pathname);
+
+      //Remvove Event Listener after un-mount
+      return () => {
+        routers.events.off("routeChangeComplete", logEvent);
+      };
+    }
+  }, []);
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
